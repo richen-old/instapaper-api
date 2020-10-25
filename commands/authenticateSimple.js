9fb27b31979fd5afd5477153ae888177a48dc9e8
richen604 <56615615+richen604@users.noreply.github.com>
2020-10-25 16:25:10 -0700

    bad push, see last commit

---
+++ commands/authenticateSimple.js
@@ -0,0 +1,15 @@
+const performRequest = require("./performRequest");
+
+const perform = require('./performRequest.js')
+const {username, password} = require('../config.json')
+
+console.log(`username: ${username}`)
+console.log(`password: ${password}`)
+
+const run = async () =>  {
+    await perform.getData(`https://www.instapaper.com/api/authenticate`, {username: username, password: password})
+    .then(data => {
+        console.log(data)
+    });
+}
+run()
